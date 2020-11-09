2394351706af5f6ad942288b6e488a10c4751201
Mạnh tý hon <peterdinh.m@gmail.com>
2020-11-09 17:24:41 +0700

    create menu

---
+++ ScorpionAccessManager/Command/UpdateViewCommand.cs
@@ -0,0 +1,40 @@
+﻿using ScorpionAccessManager.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ScorpionAccessManager.Command
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine("Manh");
+            if (parameter.ToString() == "ucCard")
+            {
+                viewModel.SelectedViewModel = new CardViewModel();
+            }
+            else
+            {
+                viewModel.SelectedViewModel = new CardHolderViewModel();
+            }
+        }
+    }
+}
--- ScorpionAccessManager/ViewModel/MainViewModel.cs
+++ ScorpionAccessManager/ViewModel/MainViewModel.cs
@@ -1,29 +1,42 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using ScorpionAccessManager.Command;
+using ScorpionAccessManager.Model;
+using ScorpionAccessManager.View;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ScorpionAccessManager.ViewModel
 {
-    class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
-        private IList<UserControl> _UCList;
+        private BaseViewModel _selectedViewModel;
 
-        public IList<UserControl> UCList
+        public BaseViewModel SelectedViewModel
         {
-            get { return _UCList; }
-            set { _UCList = value; }
+            get { return _selectedViewModel; }
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
         }
+
+        public ICommand UpdateViewCommand { get; set; }
 
         public MainViewModel()
         {
-            _UCList = new List<UserControl>
-            {
 
-            };
+            UpdateViewCommand = new UpdateViewCommand(this);
         }
-
     }
 }
